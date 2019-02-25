42d626f6a4bca4c309054722d4037b6ec76b13de
waeljawadi <waeljawadi1@gmail.com>
2019-02-26 00:29:43 +0100

    26-02-2019

--- src/component/App.js
+++ src/component/App.js
@@ -1,30 +1,17 @@
-
-import React, { Component } from 'react';
+import React, { Component } from "react"
 import Header from "./header/header"
-
 import Footer from "./footer/footer"
-
-import Home from './home/home'
-
-
+import Home from "./home/home"
 import "./App.css"
-
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-
         <Header />
-        <Home/>
-
-    
+        <Home />
         <Footer />
-
       </div>
     )
   }
 }
-
-
 export default App
