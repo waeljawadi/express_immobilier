030f002f37af150756b30427a8a1df449f4e1f15
hamzatroudi <troudi.hamza90@gmail.com>
2019-02-26 16:01:53 +0100

    catalogue
     catalogue + style

--- src/component/catalogue/catalogue.js
+++ src/component/catalogue/catalogue.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import './catalogue.css'
 
 const Catalogue=(props)=>{
     return(<div className="catalogue">
-    <div><img src ={props.item.image} className="imglist"/></div>
-    <div className="titlehouse"><p> {props.item.title}</p></div>
-    <div className="descriptionhouse"><p> {item.description}</p></div>
+    
+    <div className="imglist"><img src ={props.item.image} className="imglist"/>
+    
+    
+    <button type="button" className="btn btn-success open">View Property</button>
+    </div>
+   
+    <i class="fa fa-heart"></i>
+    
+    
+    
+
+    <div className="titlehouse"><h4 className="ptitlehouse"> {props.item.title}</h4></div>
+    <div className="descriptionhouse"><p className="pdescriptionhouse"> {props.item.description}</p></div>
+    <div className="surfacehouse"><p className="psurfacehouse"> {props.item.surface} <b>m</b><sup>2</sup></p></div>
+    <div className="prixhouse"><p className="pprixhouse"> {props.item.prix} TND</p></div>
    
     
     </div>
