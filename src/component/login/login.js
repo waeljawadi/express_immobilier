ffa5e248add2afa78dc7576c079ece6ea8d6cb94
waeljawadi <waeljawadi1@gmail.com>
2019-02-26 12:03:56 +0100

    Create login.js

---
+++ src/component/login/login.js
@@ -0,0 +1,9 @@
+import Raect from "react"
+import { Component } from "react"
+
+class Login extends Component {
+  render() {
+    return <React.Fragment>Login</React.Fragment>
+  }
+}
+export default Login
