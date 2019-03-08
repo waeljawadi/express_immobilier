460a44524df6e47dc1bd33e2eb20b3c9b28e71bb
waeljawadi <waeljawadi1@gmail.com>
2019-03-08 16:45:50 +0100

    ok

--- src/component/administration/catalogue/ajout/ajout.js
+++ src/component/administration/catalogue/ajout/ajout.js
@@ -5,9 +5,6 @@ import './style/style.css'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import "react-tabs/style/react-tabs.css";
 import 'rc-input-number/assets/index.css';
-import InputNumber from 'rc-input-number';
-
-
 class Ajout extends Component {
 
 
@@ -17,15 +14,19 @@ class Ajout extends Component {
       this.state = { 
         reference: Math.trunc(Date.now() * Math.random() * Math.random()),
         title : '',
-        prix : ''
+        prix : '' ,
+        surface : ''
+
        }
     }
   }
 
-handlechange = (event) =>
-{
-  this.setState({[event.target.name]:event.target.value})
-}
+  handleNumbers = (evt) => {
+    this.setState({ [evt.target.name]:(evt.target.validity.valid) ? evt.target.value : this.state[evt.target.name] });
+  }
+  handlechange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
   render() {
 
     
@@ -33,52 +34,111 @@ handlechange = (event) =>
 
     return (
       <React.Fragment>
-<div className={`content-wrapper ${_css_.padds}`}>
-<Tabs>
-    <TabList>
-      <Tab>Information</Tab>
-      <Tab>Title 2</Tab>
-    </TabList>
+        <div className={`content-wrapper ${_css_.padds}`}>
+          <Tabs>
+            <TabList>
+              <Tab>Information</Tab>
+              <Tab>Title 2</Tab>
+            </TabList>
 
-    <TabPanel>   
-<div class="container ajouter">
-  <form>
-    <div class="row">
-    <div class="col-half">
-      <div class="input-group input-group-icon">
-        <input type="text" placeholder="Reference" name="reference" value={this.state.reference} disabled/>
-        <div class="input-icon"><i class="fa fa-user"></i></div>
-      </div>
-     </div>
-     <div class="col-half">
-      <div class="input-group input-group-icon">
-        <input type="text" placeholder="Title" name="title" value={this.state.title} onChange={this.handlechange} />
-        <div class="input-icon"><i class="fa fa-user"></i></div>
-      </div>
-     </div>
+            <TabPanel>
+              <div className="container ajouter">
+                <form>
+                  <div className="row">
+                    <div className="col-half">
+                      <div className="input-group input-group-icon">
+                        <input
+                          type="text"
+                          placeholder="Reference"
+                          name="reference"
+                          value={`Reference: ${this.state.reference}`}
+                          disabled
+                        />
+                        <div className="input-icon">
+                          <i className="fa fa-user" />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-half">
+                      <div className="input-group input-group-icon">
+                        <input
+                          type="text"
+                          placeholder="Untitulé du bien"
+                          name="title"
+                          value={this.state.title}
+                          onChange={this.handlechange}
+                        />
+                        <div className="input-icon">
+                          <i className="fa fa-user" />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-half">
+                      <div className="input-group input-group-icon">
+                        <input
+                          type="text"
+                          pattern="[0-9]*"
+                          placeholder="Prix du bien en dinar"
+                          name="prix"
+                          onChange={this.handleNumbers}
+                          value={this.state.prix}
+                        />
+                        <div className="input-icon">
+                          <i className="fa fa-user" />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-half">
+                      <div className="input-group input-group-icon">
+                        <input
+                          type="text"
+                          pattern="[0-9]*"
+                          placeholder="Surface du bien en m²"
+                          onChange={this.handleNumbers}
+                          name="surface"
+                          value={this.state.surface}
+                        />
+                        <div className="input-icon">
+                          <i className="fa fa-user" />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
 
-     <div class="col-half">
-      <div class="input-group input-group-icon">
-        <input type="text" pattern="\d*" placeholder="Prix en dinars" name="prix" value={this.state.prix} onChange={this.handlechange} />
-        <div class="input-icon"><i class="fa fa-user"></i></div>
-      </div>
-     </div>
+                  <div className="row">
+                  <div className="col-half">
+                  <div className="input-group input-group-icon">
+                      <input className="form-control" type="date"  placeholder="ggg" name="date_construction" />
 
-     <InputNumber
-          min={0}
-          value={this.state.prix}
-          onChange={this.handlechange}
-          name="prix"
-        />
-
-    </div>
-  </form>
+                        <div className="input-icon">
+                          <i className="fa fa-user" />
+                        </div>
+                      </div>
 </div>
-    </TabPanel>
-    <TabPanel>dfgdfg</TabPanel> 
-  </Tabs>
+
+<div className="col-half">
+                  </div>
 </div>
 
+                </form>
+              </div>
+              {this.state.prix}
+              {this.state.surface}
+
+            
+
+
+
+
+             
+    
+            </TabPanel>
+
+            <TabPanel>dfgdfg</TabPanel>
+          </Tabs>
+        </div>
       </React.Fragment>
     )
   }
