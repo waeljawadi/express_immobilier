a18dda7f70b9e343e290e8160be4b1608cf3a2ff
waeljawadi <waeljawadi1@gmail.com>
2019-03-08 14:09:09 +0100

    Ajouter

--- src/component/administration/catalogue/ajout/ajout.js
+++ src/component/administration/catalogue/ajout/ajout.js
@@ -1,36 +1,82 @@
 import React from "react"
 import { Component } from "react"
-
 import _css_ from "../../style/style.module.css"
-import _tabs_ from './style/style.module.css'
+import './style/style.css'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import "react-tabs/style/react-tabs.css";
+import 'rc-input-number/assets/index.css';
+import InputNumber from 'rc-input-number';
 
 
 class Ajout extends Component {
+
+
+  constructor(props) {
+    super(props)
+    {
+      this.state = { 
+        reference: Math.trunc(Date.now() * Math.random() * Math.random()),
+        title : '',
+        prix : ''
+       }
+    }
+  }
+
+handlechange = (event) =>
+{
+  this.setState({[event.target.name]:event.target.value})
+}
   render() {
+
+    
+
+
     return (
       <React.Fragment>
 <div className={`content-wrapper ${_css_.padds}`}>
-<div>    
-        <ul className="nav nav-tabs" role="tablist">
-          <li className={`nav-item ${_tabs_.navitemedit}`}>
-            <a className="nav-link active" href="#profile" role="tab" data-toggle="tab">profile</a>
-          </li>
-           <li className={`nav-item ${_tabs_.navitemedit}`}>
-            <a className="nav-link" href="#buzz" role="tab" data-toggle="tab">buzz
-            
-            </a>
-          </li>
-           <li className={`nav-item ${_tabs_.navitemedit}`}>
-            <a className="nav-link" href="#references" role="tab" data-toggle="tab">references</a>
-          </li>
-        </ul>   
-        <div className="tab-content">
-          <div role="tabpanel" className="tab-pane fade in active" id="profile">...</div>
-          <div role="tabpanel" className="tab-pane fade" id="buzz">
-          </div>
-          <div role="tabpanel" className="tab-pane fade" id="references">ccc</div>
-        </div>
+<Tabs>
+    <TabList>
+      <Tab>Information</Tab>
+      <Tab>Title 2</Tab>
+    </TabList>
+
+    <TabPanel>   
+<div class="container ajouter">
+  <form>
+    <div class="row">
+    <div class="col-half">
+      <div class="input-group input-group-icon">
+        <input type="text" placeholder="Reference" name="reference" value={this.state.reference} disabled/>
+        <div class="input-icon"><i class="fa fa-user"></i></div>
       </div>
+     </div>
+     <div class="col-half">
+      <div class="input-group input-group-icon">
+        <input type="text" placeholder="Title" name="title" value={this.state.title} onChange={this.handlechange} />
+        <div class="input-icon"><i class="fa fa-user"></i></div>
+      </div>
+     </div>
+
+     <div class="col-half">
+      <div class="input-group input-group-icon">
+        <input type="text" pattern="\d*" placeholder="Prix en dinars" name="prix" value={this.state.prix} onChange={this.handlechange} />
+        <div class="input-icon"><i class="fa fa-user"></i></div>
+      </div>
+     </div>
+
+     <InputNumber
+          min={0}
+          value={this.state.prix}
+          onChange={this.handlechange}
+          name="prix"
+        />
+
+    </div>
+  </form>
+</div>
+    </TabPanel>
+    <TabPanel>dfgdfg</TabPanel> 
+  </Tabs>
 </div>
 
       </React.Fragment>
