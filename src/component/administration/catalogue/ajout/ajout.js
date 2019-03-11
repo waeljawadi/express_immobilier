65c444aa0903de12ae7cf8f2e366d0f9acd36f99
waeljawadi <waeljawadi1@gmail.com>
2019-03-11 15:48:05 +0100

    Update ajout.js

--- src/component/administration/catalogue/ajout/ajout.js
+++ src/component/administration/catalogue/ajout/ajout.js
@@ -6,8 +6,6 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import "react-tabs/style/react-tabs.css";
 import 'rc-input-number/assets/index.css';
 class Ajout extends Component {
-
-
   constructor(props) {
     super(props)
     {
@@ -17,23 +15,30 @@ class Ajout extends Component {
         prix : '' ,
         surface : '',
         date_construction:'',
-        type_immobilier:''
-
+        type_immobilier:'',
+        adresse: '',
+        ville:'',
+        dcourte:'',
+        dcomplet:'',
+        vendu: false,
+        nouveaute: false,
+        desactiver: false,
+        enavant: false,
        }
     }
   }
-
   handleNumbers = (evt) => {
     this.setState({ [evt.target.name]:(evt.target.validity.valid) ? evt.target.value : this.state[evt.target.name] });
   }
   handlechange = (event) => {
     this.setState({ [event.target.name]: event.target.value })
   }
+  toggleChange = (e) => {
+    this.setState({
+      [e.target.name]: !this.state[e.target.name]
+    });
+  }
   render() {
-
-    
-
-
     return (
       <React.Fragment>
         <div className={`content-wrapper ${_css_.padds}`}>
@@ -44,7 +49,6 @@ class Ajout extends Component {
               <Tab>Etiquette</Tab>
               <Tab>Images</Tab>
             </TabList>
-
             <TabPanel>
               <div className="container ajouter">
                 <form>
@@ -110,140 +114,118 @@ class Ajout extends Component {
                       </div>
                     </div>
                   </div>
-
                   <div className="row">
-                  <div className="col-half">
-                  <div className="input-group input-group-icon">
-                      <input  type="date" name="date_construction"  onChange={this.handlechange}
-                      value={this.state.date_construction}
-                       />
-
+                    <div className="col-half">
+                      <div className="input-group input-group-icon">
+                        <input
+                          type="date"
+                          name="date_construction"
+                          onChange={this.handlechange}
+                          value={this.state.date_construction}
+                        />
                         <div className="input-icon">
                           <i className="fa fa-user" />
                         </div>
                       </div>
-</div>
-
-<div className="col-half">
-
-
-
-
-<div className="input-group input-group-icon">
-   <select 
-   name="type_immobilier"
-   value={this.state.type_immobilier}
-    onChange={this.handlechange}
-   >
-   <option value="" disabled selected>Choisir type d'immobilier</option>
-     <option>Maison</option>
-     <option>Appartement</option>
-     <option>Villa</option>
-   </select>
-   <div className="input-icon">
+                    </div>
+                    <div className="col-half">
+                      <div className="input-group input-group-icon">
+                        <select
+                          name="type_immobilier"
+                          value={this.state.type_immobilier}
+                          onChange={this.handlechange}
+                        >
+                          <option value="" disabled selected>
+                            Choisir type d'immobilier
+                          </option>
+                          <option>Maison</option>
+                          <option>Appartement</option>
+                          <option>Villa</option>
+                        </select>
+                        <div className="input-icon">
                           <i className="fa fa-user" />
                         </div>
-</div>
-
-
-
-
-
-
-
+                      </div>
+                    </div>
                   </div>
-</div>
-
+                  <div className="row">
+                    <div className="col-half">
+                      <div className="input-group input-group-icon">
+                        <input
+                          type="text"
+                          placeholder="Adresse"
+                          onChange={this.handlechange}
+                          name="adresse"
+                          value={this.state.adresse}
+                        />
+                        <div className="input-icon">
+                          <i className="fa fa-user" />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-half">
+                    <div className="input-group input-group-icon">
+                        <input
+                          type="text"
+                          placeholder="Ville"
+                          onChange={this.handlechange}
+                          name="ville"
+                          value={this.state.ville}
+                        />
+                        <div className="input-icon">
+                          <i className="fa fa-user" />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </form>
               </div>
-              
-
-
-
-
-             
-    
             </TabPanel>
             <TabPanel>
-
-              
-            <div className="container ajouter">
+              <div className="container ajouter">
                 <form>
-              <div className="row">
-                
-                  <div className="input-group input-group-icon">
-                  <textarea class="form-control" placeholder="Description courte"></textarea>
-                   
-                 
-
-                </div>
-                <div className="input-group input-group-icon area_height">
-                  <textarea placeholder="Description detaillé" class="form-control"></textarea>
-                   
-                 
-
-
-                </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                
-</div>
-</form>
-</div>
-
+                  <div className="row">
+                    <div className="input-group input-group-icon">
+                      <textarea
+                        onChange={this.handlechange}
+                        class="form-control"
+                        placeholder="Description courte"
+                        name="dcourte"
+                        value={this.state.dcourte}
+                      />
+                    </div>
+                    <div className="input-group input-group-icon area_height">
+                      <textarea
+                        placeholder="Description detaillé"
+                        class="form-control"
+                        name="dcomplet"
+                        value={this.state.dcomplet}
+                        onChange={this.handlechange}
+                      />
+                    </div>
+                  </div>
+                </form>
+              </div>
             </TabPanel>
             <TabPanel>
-
-
-
-
-            <div class="boxes">
-  <input type="checkbox" id="vendu" />
-  <label for="vendu">Vendu</label>
-  <input type="checkbox" id="nouveaute" />
-  <label for="nouveaute">Nouveauté</label>
-  <input type="checkbox" id="desactiver" />
-  <label for="desactiver">Desactiver</label>
-  <input type="checkbox" id="enavant" />
-  <label for="enavant">Mettre en avant</label>
-</div>
-
-
-
-
+              <div class="boxes">
+                <input type="checkbox" id="vendu"  name="vendu"  checked={this.state.vendu} onChange={this.toggleChange} />
+                <label for="vendu">Vendu</label>
+                <input type="checkbox" id="nouveaute" name="nouveaute" checked={this.state.nouveaute} onChange={this.toggleChange} />
+                <label for="nouveaute">Nouveauté</label>
+                <input type="checkbox" id="desactiver" name="desactiver" checked={this.state.desactiver} onChange={this.toggleChange} />
+                <label for="desactiver">Desactiver</label>
+                <input type="checkbox" id="enavant" name="enavant" checked={this.state.enavant} onChange={this.toggleChange} />
+                <label for="enavant">Mettre en avant</label>
+              </div>
             </TabPanel>
           </Tabs>
-
-          <button type="button" class="btn btn-success">Ajouter immobilier</button>
-
-
-
+          <button type="button" class="btn btn-success">
+            Ajouter immobilier
+          </button>
         </div>
       </React.Fragment>
     )
   }
 }
-
 export default Ajout
