90182c6cafb53de368c7f491392d92c17832cb8e
waeljawadi <waeljawadi1@gmail.com>
2019-03-14 18:23:38 +0100

    ok

--- backend/server.js
+++ backend/server.js
@@ -106,21 +106,21 @@ app.get('/getimmo',(req,res)=>{
     })
 
 
-    app.put('/showimmo/:id',(req,res)=>{
+    app.put('/isactive/:id/:desactiver',(req,res)=>{
       let id=ObjectID(req.params.id)
-  db.collection('immo').findOneAndUpdate({_id:id,desactiver:false},{$set:{desactiver:true}},(err,data)=>{
-      if(err) res.send('can not edit the immo')
-      else res.send (data)
-  })
+      let desactiver= req.params.desactiver
+      let x;
+      (desactiver == 0) ? x = 1 : x = 0
+  db.collection('immo').findOneAndUpdate({_id:id},{$set:{desactiver:x}},(err,data)=>{
+      (err) ? res.send('can not edit the contacts') : res.send (data)
   })
   
   
-
  
 
 })
 
-
+})
 const port = process.env.PORT || 3070
 app.listen(port, err => {
   err ? console.log("Server is down") : console.log("Server up and runing")
--- src/component/administration/catalogue/list/single_immo.js
+++ src/component/administration/catalogue/list/single_immo.js
@@ -1,130 +1,100 @@
-import React, { Component } from 'react';
-import axios from 'axios'
-import {connect} from 'react-redux'
-import './listimmo.css'
-
+import React, { Component } from "react"
+import axios from "axios"
+import { connect } from "react-redux"
+import "./listimmo.css"
 class SingleImmo extends Component {
-   
+  deleteimmobilier = () => {
+    const { item } = this.props
+    axios
+      .delete(`/deleteimmo/${item._id}`)
+      .then(() => this.props.deleteImmoReducer(item._id))
+      .catch(err => alert(err))
+  }
+  statue = () => {
+    if (this.props.item.vendu == 1) return "vendu"
+    else return "disponible"
+  }
 
-    deleteimmobilier=()=>
-    {  const {item} = this.props
-    axios.delete(`/deleteimmo/${item._id}`)   
-  .then(()=>this.props. deleteImmoReducer(item._id)) 
-  .catch((err)=>alert(err)) 
-    }
 
-
-// suprrimmo=()=>
-// {  const {item} = this.props
-// axios.delete(`/deleteimmo/${item._id}`)   
-// .then('') 
-// .catch((err)=>alert(err)) 
-// }
-
-show=()=>
-{
-const {item}=this.props
-axios.put(`/showimmo/${item._id}`)
-.then('')
-.catch((err)=>alert(err))
-
+  componentDidUpdate=()=>{
+    axios.get('/getimmo').then((res)=>this.props.updateImmoReducer(res.data))
 }
 
 
-    statue=()=>{
-      if ((this.props.item.vendu)==1 )
-    
-       return 'vendu'
-      
-        else 
-          return 'disponible'
-      }
-
-   
-
-      
-    
-    
-
-
-    render() { 
+  isactive=()=>
+    {
       const {item}=this.props
-      
-
-        return ( <React.Fragment> 
-<tr>
-                        <td>
-                          <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt />
-                          <a href="#" className="user-link">{item.title}</a>
-                          <span className="user-subhead">Admin</span>
-                        </td>
-                        <td>
-                          {item.date_construction}
-                        </td>
-                        <td className="text-center">
-                          <span className="label label-default">{this.statue()} </span>
-                        </td>
-                        <td>
-                          <a href="#">{item.adresse}</a>
-                        </td>
-                        <td style={{width: '20%'}}>
-                          <a href="#" className="table-link">
-                          <span class="fa-stack">
-                          <i class="fa fa-camera fa-stack-1x" ></i>
-                         <i class="fa fa-ban fa-stack-2x" 
-                            onClick={this.show}
-                           >
-                          </i>
-                         </span>
-                          </a>
-                          <a href="#" className="table-link">
-                            <span className="fa-stack">
-                              <i className="fa fa-square fa-stack-2x" />
-                              <i className="fa fa-pencil fa-stack-1x fa-inverse" />
-                            </span>
-                          </a>
-                          <a href="#" className="table-link danger">
-                            <span className="fa-stack">
-                              <i className="fa fa-square fa-stack-2x"  />
-                              <i className="fa fa-trash-o fa-stack-1x fa-inverse"   onClick={this.deleteimmobilier}></i>>
-                            </span>
-                          </a>
-                          
-                        </td>
-                        
-                      </tr>
-                     
-
-
-
-
-
-
-        </React.Fragment> );
+       axios.put(`/isactive/${item._id}/${item.desactiver}`) 
+      .then() 
+      .catch()
     }
+
+
+
+
+
+
+  render() {
+    const { item } = this.props
+    return (
+      <React.Fragment>
+        <tr>
+          <td>
+            <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt />
+            <a href="#" className="user-link">
+              {item.title}
+            </a>
+            <span className="user-subhead">Admin</span>
+          </td>
+          <td>{item.date_construction}</td>
+          <td className="text-center">
+            <span className="label label-default">{this.statue()} </span>
+          </td>
+          <td>
+            <a href="#">{item.adresse}</a>
+          </td>
+          <td style={{ width: "20%" }}>
+            <a href="#" className="table-link">
+              <span class="fa-stack">
+                <i class="fa fa-camera fa-stack-1x" />
+                <i class="fa fa-ban fa-stack-2x" 
+                onClick={this.isactive} 
+                />
+              </span>
+            </a>
+            <a href="#" className="table-link">
+              <span className="fa-stack">
+                <i className="fa fa-square fa-stack-2x" />
+                <i className="fa fa-pencil fa-stack-1x fa-inverse" />
+              </span>
+            </a>
+            <a href="#" className="table-link danger">
+              <span className="fa-stack">
+                <i className="fa fa-square fa-stack-2x" />
+                <i
+                  className="fa fa-trash-o fa-stack-1x fa-inverse"
+                  onClick={this.deleteimmobilier}
+                />
+                >
+              </span>
+            </a>
+          </td>
+        </tr>
+      </React.Fragment>
+    )
+  }
 }
- 
-const mapDispatchToProps=(dispatch)=>
-{
-    return {
-        deleteImmoReducer:_id=>
-        {
-            dispatch({
-                type:'REMOVE_IMMO',
-                _id
-            })
-        }
+const mapDispatchToProps = dispatch => {
+  return {
+    deleteImmoReducer: _id => {
+      dispatch({
+        type: "REMOVE_IMMO",
+        _id
+      })
     }
+  }
 }
-
-export default connect(null,mapDispatchToProps)(SingleImmo);
-
-
-
-
-
-
-
-
-
-
+export default connect(
+  null,
+  mapDispatchToProps
+)(SingleImmo)
